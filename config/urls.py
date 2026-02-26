7429a0807d3c04787c1bc6439abc2404b052e5ce
Keshav Kumar <keshavstm4@gmail.com>
2026-02-26 15:58:56 +0530

    feat: Add login page and implement login functionality with API integration

--- config/urls.py
+++ config/urls.py
@@ -12,6 +12,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/index.html"), name="home"),
+    path("login/", TemplateView.as_view(template_name="pages/login.html"), name="login"),
     path(
         "dashboard/",
         TemplateView.as_view(template_name="pages/dashboard.html"),
